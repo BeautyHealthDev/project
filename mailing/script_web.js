a4e0905e370986ce77ae5d0dacb2879f4c43dcac
BeautyHealthDev <beautyhealthdevelopment@gmail.com>
2026-01-28 16:01:37 +0300

    Update script_web.js

--- mailing/script_web.js
+++ mailing/script_web.js
@@ -1,7 +1,7 @@
 const { chromium } = require('playwright');
 const fs = require('fs');
 const loginPage = 'https://faberlic.com/ru/ru/login';
-const reportPage = 'https://faberlic.com/rssreports/otchet.php?linkreport=/ReportServer/Pages/ReportViewer.aspx?%2fRP_distributor%2fReportMLM2MC&rs:Format=XML&nnumber=737127362&period=30000000521&nullsum=0&ownstructure=1&hidezombnull=0&lang=RU';
+const reportPage = 'https://faberlic.com/rssreports/otchet.php?linkreport=/ReportServer/Pages/ReportViewer.aspx?%2fRP_distributor%2fReportMLM2MC&rs:Command=Render&rc:Stylesheet=htmlviewer&nnumber=737127362&period=30000000521&nullsum=0&ownstructure=1&hidezombnull=0&lang=RU';
 
 (async () => {
   const browser = await chromium.launch();
@@ -17,16 +17,29 @@ const reportPage = 'https://faberlic.com/rssreports/otchet.php?linkreport=/Repor
   await page.waitForNavigation();
   console.log(`Страница ${loginPage} успешно загружена`);
 
-  // 2. Переход на страницу с XML
-  // Браузер сохранит сессию (cookies), поэтому повторный логин не потребуется
-  const response = await page.goto(reportPage, { timeout: 120000 });
-  console.log(`Страница ${reportPage} успешно загружена`);
+  // 2. Переход на страницу с отчетом
+  await page.goto(reportPage);
 
-  // 3. Получаем содержимое XML и сохраняем в файл
+  // 3. Ожидание события загрузки и вызов JS функции
+  // Мы запускаем ожидание ПЕРЕД тем, как вызвать команду экспорта
+  const downloadPromise = page.waitForEvent('download');
+  
+  await page.evaluate(() => {
+    // Пауза
+    console.log(`Пауза - старт`);
+    await page.waitForTimeout(10000);
+    console.log(`Пауза - финиш`);
+    // Вызываем встроенную функцию ReportViewer
+    $find('ReportViewerControl').exportReport('XML');
+  });
+
+  const download = await downloadPromise;
+
+  // 4. Сохранение скачанного файла
   const path = './ReportMLM2MC.xml';
-  const xmlContent = await response.text();
-  fs.writeFileSync(path, xmlContent);
-  console.log(`XML-файл с отчетом успешно скачан и сохранен в: ${path}`);
+  await download.saveAs(path);
+  
+  console.log(`Файл успешно скачан и сохранен в: ${path}`);
 
   await browser.close();
 })();
