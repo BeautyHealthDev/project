952d41bfe3b92f3b6ff9e47aab0733c8f0c7833a
BeautyHealthDev <beautyhealthdevelopment@gmail.com>
2026-01-28 13:54:42 +0300

    Update script_web.js

--- mailing/script_web.js
+++ mailing/script_web.js
@@ -1,7 +1,7 @@
 const { chromium } = require('playwright');
 const fs = require('fs');
 const loginPage = 'https://faberlic.com/ru/ru/login';
-const reportPage = 'https://faberlic.com/rssreports/otchet.php?linkreport=/ReportServer/Pages/ReportViewer.aspx?%2fRP_distributor%2fReportMLM2MC&rs:Command=Render&rc:Stylesheet=htmlviewer&nnumber=737127362&period=30000000521&nullsum=1&ownstructure=0&hidezombnull=0&lang=RU';
+const reportPage = 'https://faberlic.com/rssreports/otchet.php?linkreport=/ReportServer/Pages/ReportViewer.aspx?%2fRP_distributor%2fReportMLM2MC&rs:Format=XML&nnumber=737127362&period=30000000521&nullsum=1&ownstructure=0&hidezombnull=0&lang=RU';
 
 (async () => {
   const browser = await chromium.launch();
@@ -13,26 +13,17 @@ const reportPage = 'https://faberlic.com/rssreports/otchet.php?linkreport=/Repor
   await page.fill('#login', process.env.USER);
   await page.fill('#password', process.env.PASS);
   await page.click('button[dt="login_signInBtn"]');
+  // Ждем завершения навигации после клика
   await page.waitForNavigation();
 
-  // 2. Переход на страницу с отчетом
-  await page.goto(reportPage);
+  // 2. Переход на страницу с XML
+  // Браузер сохранит сессию (cookies), поэтому повторный логин не потребуется
+  const response = await page.goto(reportPage);
 
-  // 3. Ожидание события загрузки и вызов JS функции
-  // Мы запускаем ожидание ПЕРЕД тем, как вызвать команду экспорта
-  const downloadPromise = page.waitForEvent('download');
-  
-  await page.evaluate(() => {
-    // Вызываем встроенную функцию ReportViewer
-    $find('ReportViewerControl').exportReport('XML');
-  });
-
-  const download = await downloadPromise;
-
-  // 4. Сохранение скачанного файла
+  // 3. Получаем содержимое XML и сохраняем в файл
   const path = './ReportMLM2MC.xml';
-  await download.saveAs(path);
-  
+  const xmlContent = await response.text();
+  fs.writeFileSync(path, xmlContent);
   console.log(`Файл успешно скачан и сохранен в: ${path}`);
 
   await browser.close();
