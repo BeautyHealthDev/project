cec733077554f8fcb4dc7be10c593340f311de0b
BeautyHealthDev <beautyhealthdevelopment@gmail.com>
2026-01-31 00:06:49 +0300

    Update script_load_ConsultantTreeMLM.js

--- mailing/script_load_ConsultantTreeMLM.js
+++ mailing/script_load_ConsultantTreeMLM.js
@@ -1,14 +1,13 @@
 const { chromium } = require('playwright');
 const fs = require('fs');
 const loginPage = 'https://faberlic.com/ru/ru/login';
+const repInitPage = 'https://faberlic.com/index.php?option=com_edit&view=edit&listid=559&Itemid=5392';
 const nnumber = process.env.USER;
 const period = process.env.PERIOD;
 const nullsum = process.env.NULLSUM;
 const ownstructure = process.env.OWNSTRUCTURE;
 // const hidezombnull = process.env.HIDEZOMBNULL;
 // const reportPage = `https://faberlic.com/rssreports/otchet.php?linkreport=/ReportServer/Pages/ReportViewer.aspx?%2fRP_distributor%2fReportMLM2MC&rs:Command=Render&rc:Stylesheet=htmlviewer&nnumber=${nnumber}&period=${period}&nullsum=${nullsum}&ownstructure=${ownstructure}&hidezombnull=${hidezombnull}&lang=RU`;
-const reportPage = `https://faberlic.com/rssreports/otchet.php?linkreport=/ReportServer/Pages/ReportViewer.aspx?%2fRPP%2fConsultantTreeMLM&rs:Command=Render&rc:Stylesheet=htmlviewer&cons=${nnumber}&period=${period}&cur=445&ownstructure=${ownstructure}&withzero=${nullsum}&lang=RU`;
-console.log(`Сформирован URL отчета: ${reportPage}`);
 
 (async () => {
   const browser = await chromium.launch();
@@ -24,6 +23,17 @@ console.log(`Сформирован URL отчета: ${reportPage}`);
   await page.waitForNavigation();
   console.log(`Страница ${loginPage} успешно загружена`);
 
+  await page.goto(repInitPage);
+  await page.waitForNavigation();
+  console.log(`Страница запуска отчета ${repInitPage} успешно загружена`);
+
+  // Находим элемент по ID и забираем значение атрибута
+  const userId = await page.getAttribute('#gtmdata', 'data-userid');
+  console.log(`Найден userId: ${userId}`);
+  
+  const reportPage = `https://faberlic.com/rssreports/otchet.php?linkreport=/ReportServer/Pages/ReportViewer.aspx?%2fRPP%2fConsultantTreeMLM&rs:Command=Render&rc:Stylesheet=htmlviewer&cons=${userId}&period=${period}&cur=445&ownstructure=${ownstructure}&withzero=${nullsum}&lang=RU`;
+  console.log(`Сформирован URL отчета: ${reportPage}`);
+  
   // 2. Переход на страницу с отчетом
   await page.goto(reportPage);
 
@@ -35,14 +45,12 @@ console.log(`Сформирован URL отчета: ${reportPage}`);
   // Ждем, пока SSRS "отлагает" внутри
   await page.waitForFunction(() => {
     const v = window.$find("ReportViewerControl");
-    console.log(`ReportViewerControl загружается`);
     return v && !v.get_isLoading();
   });
   
   // Мы запускаем ожидание ПЕРЕД тем, как вызвать команду экспорта
   const downloadPromise = page.waitForEvent('download');
   await page.evaluate(() => {
-    console.log(`exportReport для XML запущен`);
     // Вызываем встроенную функцию ReportViewer
     $find('ReportViewerControl').exportReport('XML');
   });
