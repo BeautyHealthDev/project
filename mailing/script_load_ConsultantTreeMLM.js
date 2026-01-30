dddeaa9ef5c22f304f7aba4a165b02216f5c39cc
BeautyHealthDev <beautyhealthdevelopment@gmail.com>
2026-01-31 00:29:44 +0300

    Update script_load_ConsultantTreeMLM.js

--- mailing/script_load_ConsultantTreeMLM.js
+++ mailing/script_load_ConsultantTreeMLM.js
@@ -1,14 +1,15 @@
 const { chromium } = require('playwright');
 const fs = require('fs');
 const loginPage = 'https://faberlic.com/ru/ru/login';
-const nnumber = process.env.USER;
+// const nnumber = process.env.USER;
 const period = process.env.PERIOD;
 const nullsum = process.env.NULLSUM;
 const ownstructure = process.env.OWNSTRUCTURE;
 // const hidezombnull = process.env.HIDEZOMBNULL;
 // const reportPage = `https://faberlic.com/rssreports/otchet.php?linkreport=/ReportServer/Pages/ReportViewer.aspx?%2fRP_distributor%2fReportMLM2MC&rs:Command=Render&rc:Stylesheet=htmlviewer&nnumber=${nnumber}&period=${period}&nullsum=${nullsum}&ownstructure=${ownstructure}&hidezombnull=${hidezombnull}&lang=RU`;
-const repInitPage = `https://faberlic.com/index.php?option=com_edit&view=edit&listid=559&Itemid=5392&sponsornumber=${nnumber}`;
-
+const reportPage = `https://faberlic.com/rssreports/otchet.php?linkreport=/ReportServer/Pages/ReportViewer.aspx?%2fRPP%2fConsultantTreeMLM&rs:Command=Render&rc:Stylesheet=htmlviewer&cons=1001494180387&period=${period}&cur=445&ownstructure=${ownstructure}&withzero=${nullsum}&lang=RU`;
+console.log(`Сформирован URL отчета: ${reportPage}`);
+  
 (async () => {
   const browser = await chromium.launch();
   const context = await browser.newContext();
@@ -22,17 +23,6 @@ const repInitPage = `https://faberlic.com/index.php?option=com_edit&view=edit&li
   // Ждем завершения навигации после клика
   await page.waitForNavigation();
   console.log(`Страница ${loginPage} успешно загружена`);
-
-  await page.goto(repInitPage);
-  await page.waitForNavigation();
-  console.log(`Страница запуска отчета ${repInitPage} успешно загружена`);
-
-  // Находим элемент по ID и забираем значение атрибута
-  const userId = await page.getAttribute('#gtmdata', 'data-userid');
-  console.log(`Найден userId: ${userId}`);
-  
-  const reportPage = `https://faberlic.com/rssreports/otchet.php?linkreport=/ReportServer/Pages/ReportViewer.aspx?%2fRPP%2fConsultantTreeMLM&rs:Command=Render&rc:Stylesheet=htmlviewer&cons=${userId}&period=${period}&cur=445&ownstructure=${ownstructure}&withzero=${nullsum}&lang=RU`;
-  console.log(`Сформирован URL отчета: ${reportPage}`);
   
   // 2. Переход на страницу с отчетом
   await page.goto(reportPage);
