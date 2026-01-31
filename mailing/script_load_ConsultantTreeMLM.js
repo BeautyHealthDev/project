57eaf1c6f0fcd3c14ec929d48c4695db1f0b0b54
BeautyHealthDev <beautyhealthdevelopment@gmail.com>
2026-01-31 12:38:28 +0300

    Update script_load_ConsultantTreeMLM.js

--- mailing/script_load_ConsultantTreeMLM.js
+++ mailing/script_load_ConsultantTreeMLM.js
@@ -25,7 +25,7 @@ console.log(`Сформирован URL отчета: ${reportPage}`);
   console.log(`Страница ${loginPage} успешно загружена`);
   
   // 2. Переход на страницу с отчетом
-  await page.goto(reportPage, { timeout: 400000 });
+  await page.goto(reportPage);
 
   // 3. Ожидание события загрузки и вызов JS функции
   // Ждем первичной загрузки интерфейса
@@ -36,14 +36,8 @@ console.log(`Сформирован URL отчета: ${reportPage}`);
   await page.waitForFunction(() => {
     const v = window.$find("ReportViewerControl");
     return v && !v.get_isLoading();
-  });
-  // await page.waitForFunction(() => {
-  //   const v = window.$find && window.$find("ReportViewerControl");
-  //   // Если объект найден и он закончил загрузку — возвращаем true
-  //   return v && typeof v.get_isLoading === 'function' && !v.get_isLoading();
-  // }, { timeout: 100000 });
+  }, { timeout: 0 });
 
-  
   // Мы запускаем ожидание ПЕРЕД тем, как вызвать команду экспорта
   const downloadPromise = page.waitForEvent('download');
   await page.evaluate(() => {
